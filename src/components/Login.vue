5eb7b535979e4d2dc855c33a394e9f71f709fc06
Marotik <kitoram3@gmail.com>
2024-12-04 23:10:19 +0900

    [FEAT]Initiate Project

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const loginForm = reactive({
+  id: "",
+  password: "",
+});
+
+const login = () => {
+  console.log(loginForm);
+};
+
+</script>
+
+<template>
+  <div class="login">
+    <h1>학부모 로그인</h1>
+    <div>
+      <label for="id">아이디</label>
+      <input type="text" id="id" v-model="loginForm.id" />
+      <label for="password">비밀번호</label>
+      <input type="password" id="password" v-model="loginForm.password" />
+    </div>
+    <div>
+      <button @click="login">로그인</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
