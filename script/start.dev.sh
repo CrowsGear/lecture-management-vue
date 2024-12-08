febe2616b27502b15c4afc63f64a09d82fc95af0
Marotik <kitoram3@gmail.com>
2024-12-09 00:40:17 +0900

    [FIX]Dockerfile Container remove upgrade

--- script/start.dev.sh
+++ script/start.dev.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # run this script to start development server
 
-# stop and remove all containers with ancestor vue-app
-docker rm -f $(docker ps -q --filter ancestor=vue-app)
+# 실행 중인 컨테이너 먼저 확인
+if [ -n "$(docker ps -q --filter ancestor=vue-app)" ]; then
+    # 실행 중인 컨테이너가 있는 경우
+    echo "Removing running containers..."
+    docker rm -f "$(docker ps -q --filter ancestor=vue-app)"
+else
+    # 실행 중인 컨테이너가 없는 경우
+    echo "No running containers found. Checking stopped containers..."
+    stopped_containers=$(docker ps -a -q --filter "status=exited" --filter ancestor=vue-app)
+    if [ -n "$stopped_containers" ]; then
+        echo "Removing stopped containers..."
+        docker rm "${stopped_containers}"
+    else
+        echo "No containers with image 'vue-app' found."
+    fi
+fi
 
 
 # build and run container
