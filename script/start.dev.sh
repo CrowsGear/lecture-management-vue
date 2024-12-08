776fc2688c2e9d969752018881305d9c322d4cc5
Marotik <kitoram3@gmail.com>
2024-12-09 00:46:12 +0900

    [FIX]Dockerfile Container remove upgrade

--- script/start.dev.sh
+++ script/start.dev.sh
@@ -3,13 +3,14 @@
 
 # 실행 중인 컨테이너 먼저 확인
 if [ -n "$(docker ps -q --filter ancestor=vue-app)" ]; then
-    # 실행 중인 컨테이너가 있는 경우
     echo "Removing running containers..."
     docker rm -f "$(docker ps -q --filter ancestor=vue-app)"
+
+# 컨테이너가 실행 중이 아니라면
 else
-    # 실행 중인 컨테이너가 없는 경우
     echo "No running containers found. Checking stopped containers..."
     stopped_containers=$(docker ps -a -q --filter "status=exited" --filter ancestor=vue-app)
+
     if [ -n "$stopped_containers" ]; then
         echo "Removing stopped containers..."
         docker rm "${stopped_containers}"
@@ -20,6 +21,12 @@ fi
 
 
 # build and run container
-# $1 첫번째 인자로 포트를 받아온다.
 docker build -t vue-app .
-docker run -d -p ${1}:80 --name vue-app-dev-container vue-app
+
+# $1 첫번째 인자로 포트를 받아온다.
+PORT="${1:-5173}"
+
+# 개발 서버 실행
+docker run -d -p "${PORT}":80 --name vue-app-dev-container vue-app
+
+echo "Development server started at http://localhost:${PORT}"
