00f1bf62194eab4d08b6e17b49013b47f2fa9cda
Marotik <kitoram3@gmail.com>
2024-12-08 23:12:26 +0900

    [MODIFY]PortNumber Arg set

--- script/start.dev.sh
+++ script/start.dev.sh
@@ -6,5 +6,6 @@ docker rm -f $(docker ps -q --filter ancestor=vue-app)
 
 
 # build and run container
+# $1 첫번째 인자로 포트를 받아온다.
 docker build -t vue-app .
-docker run -d -p 5173:80 --name vue-app-dev-container vue-app
+docker run -d -p ${1}:80 --name vue-app-dev-container vue-app
