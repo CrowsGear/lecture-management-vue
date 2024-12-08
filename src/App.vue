ee26f8888315e82d3f4a94bac38936d507f28d5b
Marotik <kitoram3@gmail.com>
2024-12-08 23:02:05 +0900

    [FEAT]DELETE Practice Files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Login from "./components/Login.vue";
-import practice from "./components/practice.vue";
 </script>
 
 <template>
   <Login />
-  <practice />
 </template>
 
 <style scoped>
