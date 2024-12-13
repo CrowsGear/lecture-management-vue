5edff0338008d89ade2fd4592a315f0d8a22b913
Marotik <kitoram3@gmail.com>
2024-12-14 00:41:19 +0900

    [FEAT]docker build.sh - push 과정 추가

--- script/build.sh
+++ script/build.sh
@@ -2,4 +2,9 @@
 # run this script to start development server
 
 # build and run container
-docker build -t vue-app .
+docker build \
+      --platform linux/amd64,linux/arm64 \
+      --tag marotik/lecture_management_vue_app .
+
+# push to docker hub
+docker push marotik/lecture_management_vue_app
