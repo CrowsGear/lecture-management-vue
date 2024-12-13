4fa5ab7dd3032a61206c1172c64437a55e273f2c
Marotik <kitoram3@gmail.com>
2024-12-14 00:42:24 +0900

    [FEAT]router 진입점 생성

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import adminRoutes from "./modules/admin";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        ...adminRoutes,
+    ],
+})
+
+export default router;
