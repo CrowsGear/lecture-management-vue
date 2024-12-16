3640bbd3621a3eade6e91a5e85934cd136962f76
Marotik <kitoram3@gmail.com>
2024-12-16 22:44:40 +0900

    [FEAT]신규의존성에 의한 진입점 수정

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-// import { configDotenv } from "dotenv";
-// configDotenv();
-
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { initTheme } from './styles/theme';
 
-createApp(App)
+const pinia = createPinia()
+const app = createApp(App)
+
+// 테마 초기화
+initTheme();
+
+app
     /* router 추가 */
     .use(router)
+
+    /* pinia 추가(state 관리) */
+    .use(pinia)
 
     /* mount */
     .mount('#app')
