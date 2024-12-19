bd05e184b4a40eeb059aca8c08bb41c423ee630b
Marotik <kitoram3@gmail.com>
2024-12-20 07:58:23 +0900

    [FEAT]공용 type 생성 및 수정

---
+++ src/types/common/form.ts
@@ -0,0 +1,21 @@
+/* 폼 필드 for Modal */
+export interface IFormField {
+  name: string;
+  label: string;
+  type: 'text' | 'number' | 'date' | 'select';
+  required?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+  options?: {
+    label: string;
+    value: string | number;
+  }[];
+}
+
+/* 폼 설정 for Modal */
+export interface IFormConfig {
+  title: string;
+  fields: IFormField[];
+  submitText?: string;
+  cancelText?: string;
+} 
