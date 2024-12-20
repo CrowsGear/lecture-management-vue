6be4114a7a251740a6813dbe5a0bd4e02b6943af
Marotik <kitoram3@gmail.com>
2024-12-20 14:27:21 +0900

    [STYLE]불필요한 공백 제거

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,18 @@
     <RouterView />
   </main>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
